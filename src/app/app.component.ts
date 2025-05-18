aa6d3d09cd55528261670ead110d51673d45069b
romerojoseing <romerojoseing@gmail.com>
2025-05-18 17:06:45 +0200

    feat(#2): Install husky eslint and prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,8 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
-  selector: 'app-root',
+  selector: 'rz-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-}
+export class AppComponent {}
